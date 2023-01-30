e9a6aa1eb5e55773d109cb4c2bdd0b8c4d752720
Hosseinreza <hrakbari77@gmail.com>
2023-01-30 06:18:05 +0330

    Add boilerplate

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on http://localhost:3000");
+});
