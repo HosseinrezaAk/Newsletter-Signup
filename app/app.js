aff00865c4011b90cab2d567ef27da59dfe8db18
Hosseinreza <hrakbari77@gmail.com>
2023-01-31 07:04:49 +0330

    Add public folder

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.get("/", function( req , res){
+    res.sendFile(__dirname + "/html/signUp.html");
+});
 
 
 
